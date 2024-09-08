d89d4edf841e8adf051d5fe8b445334fd95f9ca4
Anurag <hamsen3880@gmail.com>
2024-09-09 02:02:43 +0530

    Project Setup V0.0

---
+++ src/main/java/com/chronix/chronix_dotcom_backend/ChronixDotcomBackendApplication.java
@@ -0,0 +1,13 @@
+package com.chronix.chronix_dotcom_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChronixDotcomBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChronixDotcomBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chronix/chronix_dotcom_backend/ChronixDotcomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chronix.chronix_dotcom_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChronixDotcomBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
