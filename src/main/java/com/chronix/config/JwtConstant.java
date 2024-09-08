8312c1d3373e6805d1545160ac5a188f9c1cf08a
Anurag <hamsen3880@gmail.com>
2024-09-09 02:03:51 +0530

    Authentication V0.1

---
+++ src/main/java/com/chronix/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.chronix.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY="csdnckncnowfjwjldevwfjfrjfi3jfio";
+    public static final String JWT_HEADER="Authorization";
+}
