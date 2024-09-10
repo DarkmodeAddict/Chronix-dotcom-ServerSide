05cf148c5cdfed4c34dd4b46e0a9fe33f53aa246
Anurag <hamsen3880@gmail.com>
2024-09-11 02:07:03 +0530

    User Model V0.2

---
+++ src/main/java/com/chronix/model/Issue.java
@@ -0,0 +1,19 @@
+package com.chronix.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Entity
+@Data
+public class Issue {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    private User assignee;
+}
---
+++ src/main/java/com/chronix/service/CustomUserDetailsImplementation.java
@@ -0,0 +1,34 @@
+package com.chronix.service;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.chronix.model.User;
+import com.chronix.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsImplementation implements UserDetailsService {
+    
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override 
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+
+        if(user == null) {
+            throw new UsernameNotFoundException("User not found with email: " + username);
+        }
+        List<GrantedAuthority> authorities = new ArrayList<>();
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), 
+            user.getPassword(), authorities);
+    }
+}
